cdde483e505cccc3878a73238390e3b144af7916
jwwitt <yankeewitt@gmail.com>
2021-12-27 21:21:15 -0500

    Added field class and method to print field

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field = [[]]) {
+        this.field = field;
+    }
+}
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+]);
+
+let string_row = "";
+for (let row of myField.field) {
+    for (let item of row) {
+        string_row += item;
+    }
+    console.log(string_row);
+    string_row = '';
+}
+
